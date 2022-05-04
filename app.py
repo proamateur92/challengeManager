147a473c7df019471eab8bea5461dd0ffb907bec
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-05-04 11:02:34 +0900

    add challenge write part

--- app.py
+++ app.py
@@ -11,19 +11,30 @@ from bson.objectid import ObjectId
 client = MongoClient('mongodb+srv://16thMembers:advanceToyproject@boilerplate.s8tem.mongodb.net/test?retryWrites=true&w=majority')
 db = client.hanghae
 
-now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/getChallenge', methods=['GET'])
+def showChallenge():
+    challenge_items = list(db.challenge.find({},{'_id':False}))
+    return jsonify({'challenges':challenge_items})
+
 @app.route('/saveChallenge', methods=['POST'])
 def saveChallenge():
     chal_title_receive = request.form['title']
     check_list_receive = request.form['checklist']
-    print(chal_title_receive)
-    print(check_list_receive)
-    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    doc = {
+        'title': chal_title_receive,
+        'checkList': check_list_receive,
+        'createDate': now,
+        'updateDate': ''
+    }
+    db.challenge.insert_one(doc)
+    return jsonify({'result':'success'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/challengeWrite.js
+++ static/challengeWrite.js
@@ -1,53 +1,99 @@
-let num = 0;
-const checkArr = [];
-const obj = {};
+let checkArr = [];
+let obj = {};
+
+$(document).ready(function(){
+  showChallenge();
+});
+
+function showChallenge() {
+  $.ajax({
+    type: 'GET',
+    url: '/getChallenge',
+    data: {},
+    success: (response) => {
+      const challenges = response.challenges;
+      for(let challenge of challenges) {
+        const chalTitle = challenge.title;
+        const createDate = challenge.createDate;
+        const checkLists = JSON.parse(challenge.checkList);
+        const chalHtml =
+            `<ul class="challenge-item">
+              <h2>${chalTitle}</h2></ul>`;
+        $('.challenge-container').append(chalHtml);
+        for(let checklist of checkLists) {
+          let checkHtml = '';
+          if(!checklist.isDone) {
+            checkHtml =
+                `<li class="challenge-list">
+                  <span class="done">✅ ${checklist.title}</span>
+                  <button class="btn btn-secondary">완료</button>
+                </li>`;
+          } else {
+            checkHtml =
+                `<li class="challenge-list">
+                    <span class="done">✅ ${checklist.title}</span>
+                </li>`;
+          }
+          $('.challenge-item').append(checkHtml);
+        }
+      }
+    }
+  })
+}
 
 function isBlank() {
-    const checklists = document.querySelectorAll('.checklist');
-    for(let i = 0; i < checklists.length; i++ ) {
-      if(checklists[i].value.trim() === '') {
-            alert('체크리스트를 작성해주세요.');
-            checklists[i].focus();
-            checkArr.length = 0;
-            obj.clear();
-            break;
-        }
-      obj[i] = {title: checklists[i].value, isDone: 0}
-      checkArr.push(obj[i]);
+  const checklists = document.querySelectorAll('.checklist');
+  for (let i = 0; i < checklists.length; i++) {
+    if (checklists[i].value.trim() === '') {
+      alert('체크리스트를 작성해주세요.');
+      checklists[i].focus();
+      checkArr = [];
+      obj.size = {};
+      return false;
     }
-    return checkArr;
+    obj[i] = {
+      index: i,
+      title: checklists[i].value,
+      isDone: 0
+    };
+    checkArr.push(obj[i]);
+  }
+  return checkArr;
 }
 
 function saveChal() {
-    const checklist = isBlank();
-    console.log(checklist);
-    if(!checklist || !checklist.length) {
-        return false;
-    }
-    const chal_title = $('#chal_title').val();
-    $.ajax({
-        type: "POST",
-        url: "/saveChallenge",
-        data: {
-            'title': chal_title,
-            'checklist': checklist,
-        },
-        success: function (response) {
-            console.log(response)
-        }
-    })
+  obj = {};
+  checkArr = [];
+  let checklist = isBlank();
+  if (!checklist || !checklist.length) {
+    return false;
+  }
+  const chal_title = $('#chal_title').val();
+  $.ajax({
+    type: 'POST',
+    url: '/saveChallenge',
+    data: {
+      title: chal_title,
+      checklist: JSON.stringify(checklist),
+    },
+    success: function (response) {
+      alert(response.result);
+      location.reload();
+    },
+  });
 }
 
 function addCheck() {
-    const checkContent = document.querySelector('.check-content');
-    if(num > 3) {
-        alert('체크리스트는 5개까지 추가할 수 있습니다.');
-        return false;
-    }
-    num++;
-    const checkHtml =
-        `<div>
-            <input type="text" id="checklist" class="checklist" placeholder="ex) 매일 5km 러닝" data-num=${num}>
+  let num = 0;
+  const checkContent = document.querySelector('.checklist-container');
+  if (num > 3) {
+    alert('체크리스트는 5개까지 추가할 수 있습니다.');
+    return false;
+  }
+  num++;
+  const checkHtml = `<div class="form-floating mb-3">
+                <input type="text" id="checklist" class="form-control checklist" placeholder="체크리스트 명" data-num="${num}" />
+                <label for="checklist">체크리스트 명 ex) 매일 5km 러닝</label>
         </div>`;
-    checkContent.innerHTML += checkHtml;
+  $('.checklist-container').append(checkHtml);
 }
